bb56aba36fde1529ee7d6ee991dbb40a02c26cc5
Beomman-Han <qjaaks6378@gmail.com>
2022-06-25 11:44:07 +0900

    switch 문을 amountFor() 함수로 추출

--- statement.js
+++ statement.js
@@ -8,25 +8,7 @@ function statement(invoice, plays) {
     
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${play.type}`);
-        }
+        let thisAmount = amountFor(perf, play);
         // 포인트 적립
         volumeCredits += Math.max(perf.audience - 30, 0);
         // 희극 관객 5명 마다 추가 포인트를 제공
@@ -36,6 +18,30 @@ function statement(invoice, plays) {
         result += `    ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
         totalAmount += thisAmount;
     }
+    
+    function amountFor(aPerformance, play) {
+        let result = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${play.type}`);
+        }
+        return result;
+    }
+
     result += `총액: ${format(totalAmount/100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
