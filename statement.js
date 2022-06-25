bd6c54b8f48a160cca647cea303fed04d63d295d
Beomman-Han <qjaaks6378@gmail.com>
2022-06-25 13:54:27 +0900

    volumeCredits 변수를 제거하고 함수로 추출

--- statement.js
+++ statement.js
@@ -1,15 +1,19 @@
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     
     for (let perf of invoice.performances) {
-        // 포인트 적립
-        volumeCredits += volumeCreditsFor(perf);
-
         // 청구 내역 출력
         result += `    ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
+    }
+
+    function totalVolumeCredits() {
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += volumeCreditsFor(perf);
+        }
+        return volumeCredits;
     }
 
     function volumeCreditsFor(aPerformance) {
@@ -57,7 +61,7 @@ function statement(invoice, plays) {
     }
 
     result += `총액: ${usd(totalAmount)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
 }
 
