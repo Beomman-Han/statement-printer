e09d0f2b86d976405d4015c372211329e42e8530
Beomman-Han <qjaaks6378@gmail.com>
2022-06-25 13:57:04 +0900

    totalAmount 변수를 제거하고 함수로 추출

--- statement.js
+++ statement.js
@@ -1,11 +1,17 @@
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     
     for (let perf of invoice.performances) {
         // 청구 내역 출력
         result += `    ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
+    }
+
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
     }
 
     function totalVolumeCredits() {
@@ -60,7 +66,7 @@ function statement(invoice, plays) {
         return result;
     }
 
-    result += `총액: ${usd(totalAmount)}\n`;
+    result += `총액: ${usd((totalAmount()))}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
 }
