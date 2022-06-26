3c137e61edf65a10064d45b7bd00dafa4d9f036f
Beomman-Han <qjaaks6378@gmail.com>
2022-06-26 19:02:42 +0900

    계산 단계와 포맷팅 단계를 분리

--- statement.js
+++ statement.js
@@ -1,44 +1,19 @@
 function statement(invoice, plays) {
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    
-    for (let perf of invoice.performances) {
-        // 청구 내역 출력
-        result += `    ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    }
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return renderPlainText(statementData, plays);
 
-    function totalAmount() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += amountFor(perf);
-        }
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += volumeCreditsFor(perf);
-        }
-        return result;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" == playFor(aPerformance).type) {
-            result += Math.floor(aPerformance.audience / 5);
-        }
-        
-        return result;
-    }
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-                            { style: "currency", currency: "USD",
-                              minimumFractionDigits: 2 }).format(aNumber/100);
-    }
-    
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
@@ -46,7 +21,7 @@ function statement(invoice, plays) {
     function amountFor(aPerformance) {
         let result = 0;
 
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -61,14 +36,55 @@ function statement(invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
         }
         return result;
     }
 
-    result += `총액: ${usd((totalAmount()))}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" == aPerformance.play.type) {
+            result += Math.floor(aPerformance.audience / 5);
+        }
+        
+        return result;
+    }
+
+    function totalAmount(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.amount;
+        }
+        return result;
+    }
+
+    function totalVolumeCredits(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.volumeCredits;
+        }
+        return result;
+    }
+}
+
+function renderPlainText(data, plays) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+    for (let perf of data.performances) {
+        // 청구 내역 출력
+        result += `    ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+    result += `총액: ${usd((data.totalAmount))}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
     return result;
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+                            { style: "currency", currency: "USD",
+                            minimumFractionDigits: 2 }).format(aNumber/100);
+    }
 }
 
 import invoice from "./invoices.json" assert {type: "json"};
